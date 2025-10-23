d03907f325ec31abc45ca6390dcf61e66a153bec
muhamad zaydan rafi <m.zaydan.r06@gmail.com>
2025-10-23 10:42:04 +0700

    mengikuti video yang ke 2

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,8 +1,55 @@
 <?php
 
-use CodeIgniter\Router\RouteCollection;
+namespace Config;
+
+
+$routes = Services::routes();
+
+
+
+if (is_file(SYSTEMPATH . 'Config/Routes.php')) {
+    require SYSTEMPATH . 'Config/Routes.php';
+}
+
+
+
+
+
+
+$routes->setDefaultNamespace('App\Controllers');
+$routes->setDefaultController('Home');
+$routes->setDefaultMethod('index');
+$routes->setTranslateURIDashes(false);
+$routes->set404Override();
+
+
+
+
+
+
+
+
 
 /**
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+//Admin
+$routes->get('Admin', 'Admin::index');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if (is_file(APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php')) {
+    require APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php';
+}
---
+++ app/Controllers/Auth.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controllers;
+
+class Home extends BaseController
+{
+    public function login()
+    {
+        $data = [
+            'judul' => 'Login',
+            
+        ];
+        return view('v_login',$data);
+    }
+}
